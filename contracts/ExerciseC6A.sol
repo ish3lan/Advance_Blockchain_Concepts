6efc40e6b54e62d71e673e4f42c0cf02ca6dde49
Mohammed Alshaalan <m.a.sh3lan@gmail.com>
2019-04-13 15:03:26 +0300

    add multi-party calls M of N to pause contract

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -17,6 +17,12 @@ contract ExerciseC6A {
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
 
+    uint constant M = 2;                            // 2 of 3 admins is needed to perform contract pausing
+    address[] pausingCalls = new address[](0);
+
+    event UserRegistered(address user, bool isAdmin);
+    event CallPauseContract(address user, bool isAdmin);
+
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -68,10 +74,30 @@ contract ExerciseC6A {
      */
      function pauseContract()
      external
-     requireContractOwner
      requireIsOperational
      {
-        operational = false;
+        emit CallPauseContract(msg.sender, userProfiles[msg.sender].isAdmin);
+        require(userProfiles[msg.sender].isAdmin || msg.sender == contractOwner, "Call to Pause contract from non-admin account");
+
+        bool isDuplicateCall = false;
+
+        // loop to check if call is a duplicate
+
+        for ( uint i = 0 ; i < pausingCalls.length ; i++ ){
+            if ( pausingCalls[i] == msg.sender ){
+                isDuplicateCall = true;
+                break;
+            }
+        }
+
+        require(!isDuplicateCall, "Duplicate call to pause contract from the same admin.");
+
+        pausingCalls.push(msg.sender);
+
+        if (pausingCalls.length >= M){
+            operational = false;
+            pausingCalls = new address[](0);
+        }
     }
 
 
@@ -111,6 +137,15 @@ contract ExerciseC6A {
             isRegistered: true,
             isAdmin: isAdmin
             });
+        emit UserRegistered(account ,isAdmin);
+    }
+
+
+    function isOperational()
+    external
+    view
+    returns(bool)
+    {
+        return operational;
     }
 }
-
--- test/ExerciseC6A.js
+++ test/ExerciseC6A.js
@@ -23,18 +23,52 @@ contract('ExerciseC6A', async (accounts) => {
 
   });
 
-  it('contract owner can pause the contract', async () => {
+  // it('contract owner can pause the contract', async () => {
+
+  //   // ARRANGE
+  //   let caller = accounts[0]; // This should be config.owner or accounts[0] for registering a new user
+  //   // ACT
+  //   await config.exerciseC6A.pauseContract({from: caller});
+
+  //   let contractStatus = await config.exerciseC6A.isOperational(); 
+
+  //   // ASSERT
+  //   assert.equal(contractStatus, false, "Contract owner cannot register new user");
+
+  // });
+
+
+
+  it('function call is made when multi-party threshold is reached', async () => {
 
     // ARRANGE
-    let caller = accounts[0]; // This should be config.owner or accounts[0] for registering a new user
-    let newUser = config.testAddresses[0]; 
+    let admin1 = accounts[1];
+    let admin2 = accounts[2];
+    let admin3 = accounts[3];
+    
+
+    config.exerciseC6A.CallPauseContract(function(err,res){
+      console.log(err);
+      console.log(res);
+    }); 
+
+    await config.exerciseC6A.registerUser(admin1, true, {from: config.owner});
+    await config.exerciseC6A.registerUser(admin2, true, {from: config.owner});
+    await config.exerciseC6A.registerUser(admin3, true, {from: config.owner});
+    
+    let startStatus = await config.exerciseC6A.isOperational(); 
+
+    assert.equal(startStatus, true, "Contract starting status is not operational.");
     // ACT
-    await config.exerciseC6A.pauseContract({from: caller});
-    await config.exerciseC6A.registerUser(newUser, false, {from: caller});
-    let result = await config.exerciseC6A.isUserRegistered.call(newUser); 
+    await config.exerciseC6A.pauseContract({from: admin1});
+    await config.exerciseC6A.pauseContract({from: admin2});
+    
+    let newStatus = await config.exerciseC6A.isOperational(); 
 
     // ASSERT
-    assert.equal(result, false, "Contract owner cannot register new user");
+    assert.equal(newStatus, false, "Multi-party pausing calls failed");
 
   });
+
+
 });
